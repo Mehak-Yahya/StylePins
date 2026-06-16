3f2c412048685fa64767398f60910a83aaa6f429
Mehak Yahya <kandhirmehak@gmail.com>
2026-06-17 00:09:14 +0500

    Footer

---
+++ client/src/pages/Footer.jsx
@@ -0,0 +1,43 @@
+import "../styles/Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-container">
+
+        {/* Left */}
+        <div className="footer-brand">
+          <h1>Pinterest</h1>
+          <p>© 2026 Pinterest</p>
+        </div>
+
+        {/* Column 1 */}
+        <div className="footer-column">
+          <h3>Get the app</h3>
+          <a href="/">iOS</a>
+          <a href="/">Android</a>
+        </div>
+
+        {/* Column 2 */}
+        <div className="footer-column">
+          <h3>Quick links</h3>
+          <a href="/">Explore</a>
+          <a href="/">Shop</a>
+          <a href="/">Users</a>
+          <a href="/">Collections</a>
+          <a href="/">Shopping</a>
+          <a href="/">Help Center</a>
+        </div>
+
+        {/* Column 3 */}
+        <div className="footer-column">
+          <h3>Policies</h3>
+          <a href="/">Terms of service</a>
+          <a href="/">Privacy policy</a>
+          <a href="/">Non-user notice</a>
+        </div>
+
+      </div>
+    </footer>
+  );
+}
