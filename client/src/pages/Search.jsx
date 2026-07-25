cc89de1af478f6995a228ff2b382c9f76d7bd014
Mehak Yahya <kandhirmehak@gmail.com>
2026-07-25 20:21:38 +0500

    Update

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,5 +1,5 @@
-Search.jsx
-to do 
-to add search pin functionality 
-to search pins you want to see
-browser fav content
+# Search.jsx
+# to do 
+# to add search pin functionality 
+# to search pins you want to see
+# browser fav content
